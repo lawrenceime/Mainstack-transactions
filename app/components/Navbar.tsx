ce608596593c0a046e59d77b65199bd1c03f12a5
Lawrence Ime <edetime94@gmail.com>
2024-04-21 17:06:00 +0100

    I added some images for the navbarin the assests folder

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-[95%] h-[64px] rounded-[100px] shadow-md mt-[16px] mx-auto'>
+
+    </div>
+  )
+}
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
     <div className="bg-[#EDE5FE] w-screen h-screen flex justify-center items-center">
-      <div className="w-[70%] h-[80%] bg-[#ffff]">
+      <div className="w-[75%] h-[85%] bg-[#ffff] ">
+      <Navbar/>
 
       </div>
 
